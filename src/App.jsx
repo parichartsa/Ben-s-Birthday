83589d0f46159fa267e5bd9056b3ef2d2954d1f9
parichartsa <89275029+parichartsa@users.noreply.github.com>
2025-09-07 21:33:53 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,17 @@ import Page6 from "./Page6";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/next" element={<NextPage />} />
-        <Route path="/next2" element={<Page2 />} />
-        <Route path="/page3" element={<Page3 />} />
-        <Route path="/page4" element={<Page4 />} />
-        <Route path="/page5" element={<Page5 />} />
-        <Route path="/page6" element={<Page6 />} />
-      </Routes>
-    </Router>
+<Router basename="/Ben-s-Birthday/">
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/next" element={<NextPage />} />
+    <Route path="/next2" element={<Page2 />} />
+    <Route path="/page3" element={<Page3 />} />
+    <Route path="/page4" element={<Page4 />} />
+    <Route path="/page5" element={<Page5 />} />
+    <Route path="/page6" element={<Page6 />} />
+  </Routes>
+</Router>
+
   );
 }
